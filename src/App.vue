ac31fea4ebc76f29d085e837ce0c56f1b52eaf14
admin <jack_zrj2016@163.com>
2018-03-14 16:12:36 +0800

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+ //  watch: {  
+ //    $route(to, from) {  
+ //      console.log(to);  
+ //      console.log(from);  
+ //    }  
+	// } 
+}
+</script>

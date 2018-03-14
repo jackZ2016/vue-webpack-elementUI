11b617528bda975ec4d9f8e1faea3638620e453e
admin <jack_zrj2016@163.com>
2018-03-14 22:27:53 +0800

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ export const constantRouterMap = [
 		component: () => import('@/views/login/index'),
 		hidden: true
 	},
+  {
+    path:'/404',
+    component:() => import('@/views/404'),
+    hidden: true
+  },
 	{
     path: '/',
     component: Layout,
@@ -54,6 +59,11 @@ export const constantRouterMap = [
         meta:{title:'Form',icon:'form'}
       }
     ]
+  },
+  {
+    path:'*',
+    redirect:'/404',
+    hidden:true
   }
 ]
 
